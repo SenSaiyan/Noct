f45228eecd462019aaf536905ee38f660bc74bc8
Ryan Chin <ryanchin@me.com>
2016-12-17 22:04:17 -0500

    WOW

---
+++ master/Assets/scripts/ChargeAnim.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChargeAnim : MonoBehaviour {
+	private Vector3 shake;
+	// Use this for initialization
+	void Start () {
+		shake.x = 0f;
+		shake.y = 360f;
+		shake.z = 0f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		iTween.RotateAdd (gameObject, shake, 1);
+	}
+}
---
+++ master/Assets/scripts/GameState.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameState : MonoBehaviour {
+
+	private int firstCharge;
+	public GameObject charge1;
+	public GameObject charge2;
+	public GameObject charge3;
+	public GameObject charge4;
+	public GameObject charge5;
+	public GameObject charge6;
+	public GameObject charge7;
+	public GameObject charge8;
+
+	// Use this for initialization
+	void Start () {
+		firstCharge = Random.Range(1,8);
+		if (firstCharge == 1) {
+			Instantiate (charge1);
+		}
+		if (firstCharge == 2) {
+			Instantiate (charge2);
+		}
+		if (firstCharge == 3) {
+			Instantiate (charge3);
+		}
+		if (firstCharge == 4) {
+			Instantiate (charge4);
+		}
+		if (firstCharge == 5) {
+			Instantiate (charge5);
+		}
+		if (firstCharge == 6) {
+			Instantiate (charge6);
+		}
+		if (firstCharge == 7) {
+			Instantiate (charge7);
+		}
+		if (firstCharge == 8) {
+			Instantiate (charge8);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
